bdfca4623f2dc996fd8d068832c1d9278b43964f
piyush26293 <piyushahirwar1998@gmail.com>
2024-05-07 01:03:30 +0530

    operator

--- javascriptclass/operators.js
+++ javascriptclass/operators.js
@@ -1,5 +1,33 @@
-var x = true;
-var y = "1";
+// var x = true;
+// var y = "1";
 
-console.log(x==y);
-console.log(x===y);
+// console.log(x==y);
+// console.log(x===y);
+
+
+// ternery operators
+// syntax :
+// ? :
+
+// Condition ? Expression1 : Expression2;
+
+// For example 1
+
+// var x =13;
+// (x%2==0)? console.log("ever number") : console.log("odd number");
+
+
+// For example 2
+
+var num1 =21;
+var result ="";
+
+result = (num%2==0) ? "Even" : "Odd";
+
+console.log(result);
+
+
+
+
+
+
