743fe69c045bc2c9974a465bf8c16393951554e9
piyush26293 <piyushahirwar1998@gmail.com>
2024-05-27 17:00:51 +0530

    learn new object and funciton

--- javascriptclass/function2.js
+++ javascriptclass/function2.js
@@ -1,20 +1,30 @@
-// parametrized function
-function square(num1){
-    var result = num1 * num1;
-    return result;
-}
+// // parametrized function
+// function square(num1){
+//     var result = num1 * num1;
+//     return result;
+// }
 
-var res = square(20);
-console.log(res);
-
-
+// var res = square(20);
+// console.log(res);
 
 
-function calculateTotalPrice(price, quantity){
-    var totalPrice = price * quantity;
-    return totalPrice;
 
-}
 
-var result2 = calculateTotalPrice(1000,2);
-console.log(result2);
+// function calculateTotalPrice(price, quantity){
+//     var totalPrice = price * quantity;
+//     return totalPrice;
+
+// }
+
+// var result2 = calculateTotalPrice(1000,2);
+// console.log(result2);
+
+function addAtLeastThree(a, b, c, ...numbers) { 
+    var sum = a+b+c;
+     for(var i=0; i<numbers.length; ++i) { 
+    sum += numbers[i]; 
+    } 
+    return sum; 
+    }
+    console.log( addAtLeastThree(10, 20,"") ) ; 
+
---
+++ javascriptclass/objectvyom.js
@@ -0,0 +1,78 @@
+//Object, group of primitives wrapped in {}
+// object has a key value pair
+
+// Object Literals
+const person = {};
+
+console.log(typeof person,"one");
+
+const persons = {
+    name:"sh",
+    age: 90,
+    isAdmin: true,
+    'my name': "piyush"
+};
+
+
+
+//Object constructor
+const user = new Object();
+console.log(typeof user,"two");
+
+
+
+const age = 50;
+console.log(age);
+
+const myVar= 'age';
+
+console.log(persons.age);
+console.log(persons["age"]);
+console.log(persons["name"]);
+console.log(persons[myVar]);
+console.log(persons.isAdmin);
+console.log(persons["my name"]);
+// console.log(persons.my name)  = this won't work
+
+// accessing unknown properties will print undefined
+//How to delete a property of object.
+console.log(persons);
+delete persons.isAdmin;
+console.log(persons);
+console.log(persons.isAdmin);
+
+//How to add value in objects
+//this is called property name shorthand
+
+const name ="Rohit";
+const agee = 900;
+const myUser = {
+    name,
+    age
+}
+console.log(myUser);
+
+// Proprety Existense
+console.log(persons.name ===undefined);
+console.log('myHero' in persons);
+console.log(age in persons);
+
+
+//Loop in js
+
+const marks = [23,34,56,76,34];
+for(let i=0; i<marks.length; i++){
+    console.log(marks[i]);
+}
+
+//for .... in loop
+for(let key in persons){
+    console.log(key, persons[key]);
+}
+
+
+
+
+
+
+
