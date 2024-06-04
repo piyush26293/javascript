218f87b43324276d207b2dcf5d90024efde641e4
piyush26293 <piyushahirwar1998@gmail.com>
2024-06-05 01:15:01 +0530

    learn js new methods

---
+++ javascriptclass/strings.js
@@ -0,0 +1,35 @@
+const a ="sharma";
+const b = "rohit";
+const num = 100;
+
+const c = `My name is ${b} ${a}  ${num}`;
+console.log(c);
+
+
+
+//length
+
+console.log(a.length)
+
+console.log(a.indexOf('rm'));
+console.log(a.indexOf('rmt'));
+
+console.log(a.lastIndexOf('a'));
+
+console.log(b.includes('it'));
+console.log(b.includes('itadsaf'));
+
+console.log(a.charAt(5));
+
+let full  = a + " " + b;
+console.log(full);
+
+let full1 = a.concat(a).concat(b);
+console.log(full1)
+
+console.log(a.match("ma"));
+
+
+
+
+
